69a68e4c031c25254ee440158239700f855251c9
zhangchao <zhangc0320@163.com>
2021-08-18 11:01:26 +0800

    第一次提交

---
+++ mmc_production/mmc_production/ExcelHandler.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+
+using System.Configuration;
+
+using System.Data;
+
+using System.Web;
+
+using System.Web.Security;
+
+using System.Web.UI;
+
+using System.Web.UI.HtmlControls;
+
+using System.Web.UI.WebControls;
+
+using System.Web.UI.WebControls.WebParts;
+
+using System.Data.OleDb;
+
+
+//using Excel;
+
+using System.Reflection;
+
+using System.Runtime.InteropServices;// For COMException 
+
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+
+
+
+namespace mmc_production
+{
+    class ExcelHandler
+    {
+        /*private const string COLUMN_MAC = "A";
+        private const string COLUMN_HW = "B";
+        private const string COLUMN_MODEL = "C";
+        private const string COLUMN_SN = "D";
+        private const string COLUMN_TIME = "E";
+        private const string COLUMN_STATUS = "F";
+        private const string COLUMN_MES_STATUS = "G";
+
+        private const string COLUMN_MAC_NAME = "Mac";
+        private const string COLUMN_HW_NAME = "HW version";
+        private const string COLUMN_MODEL_NAME = "Model";
+        private const string COLUMN_SN_NAME = "SN";
+        private const string COLUMN_TIME_NAME = "Time";
+        private const string COLUMN_STATUS_NAME = "Status";
+        private const string COLUMN_MES_STATUS_NAME = "MES status";*/
+        private const int COLUMN_NUMBER = 7;
+        private static readonly string[] COLUMN_INDEX_ARRAY = { "A", "B", "C", "D", "E", "F", "G" };
+        private static readonly string[] COLUMN_NAME_ARRAY = { "Mac", "HW version", "Model", "SN", "Time", "Status", "Mes status" };
+
+        private const string LOG_DIR = "log";
+
+        private const string WORK_SHEET_NAME = "MMC";
+
+        public static SpreadsheetDocument document;
+        public static Worksheet worksheet;
+
+        public static uint index = 1; //index is from 1 to 9999
+
+
+        public static readonly int NO_ERROR = 0;
+        public static readonly int ERROR_EXCEL_OPEN = -1;
+        public static readonly int ERROR_EXCEL_CREATE = -2;
+        public static readonly int ERROR_EXCEL_INSERT_HEAD = -3;
+        public static readonly int ERROR_EXCEL_INSERT_INFO = -4;
+
+        public static int openExcel()
+        {
+            string name = LOG_DIR + @"\" + System.DateTime.Now.ToString("yyyyMMdd") + "_" + ProdDataHandler.work_loc + ".xlsx";
+            if (document != null)
+            {
+                document.Close();
+            }
+            if (File.Exists(name))
+            {
+                try
+                {
+                    document = SpreadsheetDocument.Open(name, true);
+                    IEnumerable<Sheet> sheets = document.WorkbookPart.Workbook.Descendants<Sheet>().Where(s => s.Name == WORK_SHEET_NAME);
+                    if (sheets.Count() == 0)
+                    {
+                        // The specified worksheet does not exist.
+                        return ERROR_EXCEL_OPEN;
+                    }
+
+                    WorksheetPart worksheetPart = (WorksheetPart)document.WorkbookPart.GetPartById(sheets.First().Id);
+                    worksheet = worksheetPart.Worksheet;
+                    index = getLastIndex(worksheet);
+                    return NO_ERROR;
+                }catch(Exception e)
+                {
+                    return ERROR_EXCEL_OPEN;
+                }
+            }
+            else
+            {
+                try
+                {
+                    if (Directory.Exists(LOG_DIR) == false)
+                    {
+                        Directory.CreateDirectory(LOG_DIR);
+                    }
+                    document = SpreadsheetDocument.Create(name, SpreadsheetDocumentType.Workbook);
+                    // Add a WorkbookPart to the document.
+                    WorkbookPart workbookpart = document.AddWorkbookPart();
+                    workbookpart.Workbook = new Workbook();
+
+                    // Add a WorksheetPart to the WorkbookPart.
+                    WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+                    worksheetPart.Worksheet = new Worksheet(new SheetData());
+                    worksheet = worksheetPart.Worksheet;
+
+                    // Add Sheets to the Workbook.
+                    Sheets sheets = document.WorkbookPart.Workbook.
+                        AppendChild<Sheets>(new Sheets());
+
+                    // Append a new worksheet and associate it with the workbook.
+                    Sheet sheet = new Sheet()
+                    {
+                        Id = document.WorkbookPart.
+                        GetIdOfPart(worksheetPart),
+                        SheetId = 1,
+                        Name = WORK_SHEET_NAME
+                    };
+                    sheets.Append(sheet);
+
+                    workbookpart.Workbook.Save();
+
+                    insertHeadRow(worksheet);
+
+                    //index is from 1 to 9999
+                    index = 1;
+                    // Close the document.
+                    //closeExcel();
+                    return NO_ERROR;
+                }catch(Exception e)
+                {
+                    return ERROR_EXCEL_CREATE;
+                }
+            }
+        }
+
+
+        private static int insertHeadRow(Worksheet worksheet)
+        {
+            try
+            {
+                //WorkbookPart workbookpart = document.AddWorkbookPart();
+                //WorksheetPart worksheetpart = workbookpart.WorksheetParts.
+                if (worksheet == null)
+                {
+                    return ERROR_EXCEL_INSERT_HEAD;
+                }
+                SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+                //IEnumerable<SheetData> data = worksheet.Elements<SheetData>();
+
+                Row row = new Row()
+                {
+                    RowIndex = (uint)1
+                };
+                //worksheet.Append(row);
+                sheetData.Append(row);
+
+                for (int i = 0; i < COLUMN_NUMBER; ++i)
+                {
+                    string cellReference = COLUMN_INDEX_ARRAY[i] + row.RowIndex;
+
+                    Cell newCell = new Cell() { CellReference = cellReference };
+                    newCell.CellValue = new CellValue(COLUMN_NAME_ARRAY[i]);
+                    newCell.DataType = new EnumValue<CellValues>(CellValues.String);
+                    //row.InsertBefore(newCell, refCell);
+                    row.Append(newCell);
+                }
+
+                worksheet.Save();
+                //document.Close();
+                return NO_ERROR;
+            }catch(Exception e)
+            {
+                return ERROR_EXCEL_INSERT_HEAD;
+            }
+        }
+
+
+        private static uint getLastIndex(Worksheet worksheet)
+        { 
+            if (worksheet != null)
+            {
+                SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+                Row lastRow = (Row)sheetData.LastChild;
+                index = lastRow.RowIndex;
+                if (lastRow.RowIndex > 1)
+                {
+                    string reference = COLUMN_INDEX_ARRAY[3] + lastRow.RowIndex;
+                    IEnumerable<Cell> cells = worksheet.Descendants<Cell>().Where(c => c.CellReference == reference);
+                    //index: 当前最大的行数，同时检查最后一行sn 的值，如果sn中的index更大，把index设为sn中的index
+                    if (cells.Count() != 0)
+                    {
+                        Cell sn_cell = cells.First();
+                        string sn = sn_cell.InnerText;
+                        uint sn_index = uint.Parse(sn.Substring(5, 4));
+                        if (sn_index > index)
+                        {
+                            index = sn_index;
+                        }
+                    }
+                }
+                //SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+            }
+            return index;
+        }
+
+        public static int insertDeviceInfo(string mac, string hw, string model, string sn, string status, string mes_status)
+        {
+            int res = openExcel();
+            if (res != NO_ERROR) return res;
+
+            try
+            {
+                if (worksheet != null)
+                {
+                    SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+                    //IEnumerable<SheetData> data = worksheet.Elements<SheetData>();
+                    Row lastRow = (Row)sheetData.LastChild;
+                    Row row = new Row();
+                    //row.RowIndex = index + 1;
+                    {
+                        //row.RowIndex = (uint)index + 1
+                    };
+                    row.RowIndex = lastRow.RowIndex + 1;
+
+                    string[] infoArray = { mac, hw, model, sn, System.DateTime.Now.ToString("yyyyMMdd HH:mm:ss"), status, mes_status };
+                    sheetData.Append(row);
+                    for (int i = 0; i < COLUMN_NUMBER; ++i)
+                    {
+                        string cellReference = COLUMN_INDEX_ARRAY[i] + row.RowIndex;
+
+                        Cell newCell = new Cell() { CellReference = cellReference };
+                        newCell.CellValue = new CellValue(infoArray[i]);
+                        //newCell.CellValue = new CellValue(COLUMN_NAME_ARRAY[i]);
+                        newCell.DataType = new EnumValue<CellValues>(CellValues.String);
+                        //row.InsertBefore(newCell, refCell);
+                        row.Append(newCell);
+                    }
+
+                    index++;
+
+                    worksheet.Save();
+                }
+            }catch(Exception e)
+            {
+                res = ERROR_EXCEL_INSERT_INFO;
+            }
+            closeExcel();
+            return res;
+        }
+
+        public static void closeExcel()
+        {
+            if (document != null)
+            {
+                document.Close();
+                document = null;
+                worksheet = null;
+            }
+        }
+    }
+}
---
+++ mmc_production/mmc_production/ProdData.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+namespace mmc_production
+{
+    class ProdData
+    {
+        public ProdData()
+        { }
+
+        //操作员工号
+        private string account;
+
+        public string Account
+        {
+            get { return account; }
+            set { account = value;  }
+        }
+
+        //设备号
+        private string device;
+
+        public string Device
+        {
+            get { return device;  }
+            set { device = value;  }
+        }
+
+    }
+
+    //battery list
+    enum battery_enum
+    {
+        Maxwell=0,
+        Panasonic=1,
+        Taiteng=2
+    }
+
+    class ProdDataHandler
+    {
+        public static string PRODUCTION_INFO_UPDATE_PWD = "75022801";
+
+        static readonly String MES_ACCOUNT_ID_TEST = "admin";
+        static readonly String MES_DEVICE_TEST = "HW-Device-Group";
+        static readonly String MES_MAKE_ORDER_TEST = "QH-MO201605190001-1401";
+
+        static string CONFIG_FILE_NAME = "config.xml";
+        static string NODE_ROOT = "configuration";
+        static string NODE_XWD = "xwd";
+        static string NODE_ACCOUNT = "M_USERNO";
+        static string NODE_DEVICE = "M_MACHINENO";
+        static string NODE_MAKE_ORDER = "M_MO";
+        static string NODE_PRINT_LOC = "print_location";
+        static string NODE_PRODUCT_INFO = "productdata";
+        static string NODE_PRODUCT_NTC = "ntc";
+        static string NODE_PRODUCT_BATTERY = "battery";
+        static string NODE_PRODUCT_HW = "hw_version";
+        static string NODE_PRODUCT_MODLENUM = "model_num";
+        static string NODE_PRODUCT_WORK_LOC = "work_Location";
+        static string NODE_PRODUCT_FACTORY = "factory";
+
+        public static string mes_account = "";
+        public static string mes_device = "";
+        public static string mes_make_order = "";
+
+        public static string print_loc = "";
+
+        public static string ntc = "";
+        public static string battery = "";
+        public static string hw_ver = "";
+        public static string model_num = "";
+        public static string work_loc = "";
+        public static string factory = "";
+
+        //public static string sn = "";
+        public static string mesinputtest = "";
+
+
+        public ProdDataHandler()
+        {
+
+        }
+
+        public void readData()
+        {
+            XmlDocument doc = new XmlDocument();
+            if (File.Exists(AppDomain.CurrentDomain.SetupInformation.ApplicationBase+CONFIG_FILE_NAME))
+            {
+                //doc.Load(CONFIG_FILE_NAME);
+                doc.Load(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + CONFIG_FILE_NAME);
+                //doc.Load(System.Windows.Forms.Application.ExecutablePath + CONFIG_FILE_NAME);
+
+                XmlNode xn = doc.SelectSingleNode(NODE_ROOT);
+                XmlNode xe_xwd = xn[NODE_XWD];
+
+                mes_account = xe_xwd[NODE_ACCOUNT].InnerText;
+                mes_device = xe_xwd[NODE_DEVICE].InnerText;
+                mes_make_order = xe_xwd[NODE_MAKE_ORDER].InnerText;
+
+                print_loc = xn[NODE_PRINT_LOC].InnerText;
+
+                XmlNode xe_pdata = xn[NODE_PRODUCT_INFO];
+                ntc = xe_pdata[NODE_PRODUCT_NTC].InnerText;
+                battery = xe_pdata[NODE_PRODUCT_BATTERY].InnerText;
+                hw_ver = xe_pdata[NODE_PRODUCT_HW].InnerText;
+                model_num = xe_pdata[NODE_PRODUCT_MODLENUM].InnerText;
+                work_loc = xe_pdata[NODE_PRODUCT_WORK_LOC].InnerText;
+                factory = xe_pdata[NODE_PRODUCT_FACTORY].InnerText;
+            }
+            if (mes_account=="")
+            {
+                mes_account = MES_ACCOUNT_ID_TEST;
+                mes_device = MES_DEVICE_TEST;
+                mes_make_order = MES_MAKE_ORDER_TEST;
+            }
+
+        }
+
+        public void CreateNode(XmlDocument xmlDoc, XmlNode parentNode, string name, string value)
+        {
+            XmlNode node = xmlDoc.CreateNode(XmlNodeType.Element, name, null);
+            node.InnerText = value;
+            parentNode.AppendChild(node);
+        }
+
+        private void CreateXmlDoc()
+        {
+            XmlDocument doc = new XmlDocument();
+            //创建类型声明节点  
+            XmlNode node = doc.CreateXmlDeclaration("1.0", "utf-8", "");
+            doc.AppendChild(node);
+            try
+            {
+                XmlNode root = doc.CreateElement(NODE_ROOT);
+                doc.AppendChild(root);
+
+                XmlNode xe_xwd = doc.CreateElement(NODE_XWD);
+                CreateNode(doc, xe_xwd, NODE_ACCOUNT, "");
+                CreateNode(doc, xe_xwd, NODE_DEVICE, "");
+                CreateNode(doc, xe_xwd, NODE_MAKE_ORDER, "");
+                root.AppendChild(xe_xwd);
+                
+                CreateNode(doc, root, NODE_PRINT_LOC, "");
+
+                XmlNode xe_prod = doc.CreateElement(NODE_PRODUCT_INFO);
+                CreateNode(doc, xe_prod, NODE_PRODUCT_NTC, "");
+                CreateNode(doc, xe_prod, NODE_PRODUCT_BATTERY, "");
+                CreateNode(doc, xe_prod, NODE_PRODUCT_HW, "");
+                CreateNode(doc, xe_prod, NODE_PRODUCT_MODLENUM, "");
+                CreateNode(doc, xe_prod, NODE_PRODUCT_WORK_LOC, "");
+                CreateNode(doc, xe_prod, NODE_PRODUCT_FACTORY, "");
+                root.AppendChild(xe_prod);
+
+                doc.Save(CONFIG_FILE_NAME);
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        private XmlDocument getXmlDoc()
+        {
+            XmlDocument doc = new XmlDocument();
+            if (!File.Exists(CONFIG_FILE_NAME))
+            {
+                CreateXmlDoc();
+            }
+            doc.Load(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + CONFIG_FILE_NAME);
+            
+            return doc;
+        }
+
+        public bool writeMesData(string user, string hw, string order)
+        {
+            bool res = false;
+            XmlDocument doc = getXmlDoc();
+            XmlNode xn = doc.SelectSingleNode(NODE_ROOT);
+            XmlElement xe = (XmlElement)xn[NODE_XWD];
+
+            xe[NODE_ACCOUNT].InnerText = user; 
+            xe[NODE_DEVICE].InnerText = hw;
+            xe[NODE_MAKE_ORDER].InnerText = order;
+
+            mes_account = user;
+            mes_device = hw;
+            mes_make_order = order;
+            try
+            {
+                doc.Save(CONFIG_FILE_NAME);
+                res = true;
+            }
+            catch(Exception e)
+            {
+                
+            }
+           
+            return res;
+        }
+
+        public bool writePrintLoc(string path)
+        {
+            bool res = false;
+            XmlDocument doc = getXmlDoc();
+            XmlNode xn = doc.SelectSingleNode(NODE_ROOT);
+            XmlElement xe = (XmlElement)xn;
+
+            xe[NODE_PRINT_LOC].InnerText = path;
+
+            print_loc = path;
+            try
+            {
+                doc.Save(CONFIG_FILE_NAME);
+                res = true;
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return res;
+        }
+
+        public bool writeProductInfo(string ntcstr, string bat, string hw, string num, string loc, string fac)
+        {
+            bool res = false;
+            XmlDocument doc = getXmlDoc();
+            XmlNode xn = doc.SelectSingleNode(NODE_ROOT);
+            XmlElement xe = (XmlElement)xn[NODE_PRODUCT_INFO];
+
+            ntc = ntcstr;
+            battery = bat;
+            hw_ver = hw;
+            model_num = num;
+            work_loc = loc;
+            factory = fac;
+
+            try
+            {
+                xe[NODE_PRODUCT_NTC].InnerText = ntcstr;
+                xe[NODE_PRODUCT_BATTERY].InnerText = bat; 
+                xe[NODE_PRODUCT_HW].InnerText = hw.ToUpper();
+                xe[NODE_PRODUCT_MODLENUM].InnerText = num.ToUpper();
+                xe[NODE_PRODUCT_WORK_LOC].InnerText = loc.ToUpper();
+                xe[NODE_PRODUCT_FACTORY].InnerText = fac.ToUpper();
+                doc.Save(CONFIG_FILE_NAME);
+                res = true;
+                
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return res;
+        }
+
+    }//ProdDataHandler
+
+}
